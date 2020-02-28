a624ffa0f55a8d663e1689d473d22ddd7fa43139
jianren <365088870@qq.com>
2020-02-28 18:33:00 +0800

    04-导航栏完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,29 @@
 import request from "../../utils/request.js"
 Page({
   data: {
-    // 轮播图数据
-    banners: []
+    banners: [], // 轮播图数据
+    navs: [] // 导航栏数据
   },
   onLoad() {
+    // 获取轮播图数据
     request({
       url: "/home/swiperdata"
+    }).then(res => {
+      // console.log(res)
+      const {message} = res.data
+      this.setData({
+        banners: message
+      })
+    })
+
+    // 获取导航栏数据
+    request({
+      url: "/home/catitems"
     }).then(res => {
       console.log(res)
       const {message} = res.data
       this.setData({
-        banners : message
+        navs: message
       })
     })
   }
