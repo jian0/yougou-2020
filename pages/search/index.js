f1fae81d29796d80dc6e463fcea996b4e84516b9
jianren <365088870@qq.com>
2020-03-03 21:16:30 +0800

    18-搜索页面-搜索建议静态页面+获取搜索建议数据

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,3 +1,4 @@
+import request from "../../utils/request.js"
 // pages/search/index.js
 Page({
 
@@ -5,14 +6,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-    value:""
+    value:"", // 输入框内容
+    goods:[] //搜索建议数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    request({
+      url:"/goods/qsearch",
+      data:{
+        query: this.data.value
+      }
+    }).then(res=>{
+      console.log(res)
+      const {message} = res.data
+      this.setData({
+        goods: message
+      })
+      console.log(this.data.goods)
+    })
   },
+  // 输入框输入时触发
+  handleInput(e){
+    // console.log(e)
+    const {value} = e.detail
+    this.setData({
+      value
+    })
+  },
+  // 点击确定按钮时触发
+  handleEnter(){
 
+  }
 })
