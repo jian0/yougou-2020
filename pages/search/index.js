da22d4c0e920c95cc5f214ce4c597a993f5c403b
jianren <365088870@qq.com>
2020-03-03 21:28:27 +0800

    19-搜索页面-输入框聚焦/失焦事件+取消按钮事件

--- pages/search/index.js
+++ pages/search/index.js
@@ -6,38 +6,63 @@ Page({
    * 页面的初始数据
    */
   data: {
-    value:"", // 输入框内容
-    goods:[] //搜索建议数据
+    value: "", // 输入框内容
+    goods: [] //搜索建议数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    request({
-      url:"/goods/qsearch",
-      data:{
-        query: this.data.value
-      }
-    }).then(res=>{
-      console.log(res)
-      const {message} = res.data
-      this.setData({
-        goods: message
-      })
-      console.log(this.data.goods)
-    })
+  onLoad: function(options) {
+    // this.getGoods()
   },
   // 输入框输入时触发
-  handleInput(e){
+  handleInput(e) {
     // console.log(e)
-    const {value} = e.detail
+    const {
+      value
+    } = e.detail
     this.setData({
       value
     })
+    this.getGoods()
+  },
+  // 输入框失焦时触发
+  handleBlur(){
+    this.setData({
+      value:"",
+      goods:[]
+    })
+  },
+  // 点击取消按钮时触发
+  handleCancel(){
+    this.setData({
+      value:""
+    })
   },
   // 点击确定按钮时触发
-  handleEnter(){
+  handleEnter() {
+
+  },
+  // 点击关闭图标时触发
+  handleDel(){
+    
+  },
+  getGoods() {
+    request({
+      url: "/goods/qsearch",
+      data: {
+        query: this.data.value
+      }
+    }).then(res => {
+      console.log(res)
+      const {
+        message
+      } = res.data
+      this.setData({
+        goods: message
+      })
+    })
 
   }
 })
